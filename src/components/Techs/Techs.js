7c56beb3f3785a13b9c3f674b56841b4b8d34005
Vera Chernushina <chernushina.vera@gmail.com>
2022-06-07 02:17:41 +0300

    improve: Techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -4,9 +4,11 @@ const Techs = () => {
   return (
     <div className='techs'>
       <h2 className='techs__title'>Технологии</h2>
-      <span>7 технологий</span>
-      <span>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</span>
-      <ul>
+      <h3 className='techs__large-title'>7 технологий</h3>
+      <span className='techs__description'>
+        На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
+      </span>
+      <ul className='techs__list'>
         <li>HTML</li>
         <li>CSS</li>
         <li>JS</li>
