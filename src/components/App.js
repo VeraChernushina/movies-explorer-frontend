ff109dfb7df1c08299222398331c0bc0de6c874e
Vera Chernushina <chernushina.vera@gmail.com>
2022-06-15 21:46:35 +0300

    feat: routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import Main from "./Main";
+import Movies from "./Movies/Movies";
+import SavedMovies from "./SavedMovies/SavedMovies";
+import Profile from "./Profile/Profile";
+import Register from "./Register/Register";
+import Login from "./Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Switch>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route path='/saved-movies'>
+          <SavedMovies />
+        </Route>
+        <Route path='/profile'>
+          <Profile />
+        </Route>
+        <Route path='/signup'>
+          <Register />
+        </Route>
+        <Route path='/signin'>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,29 +3,15 @@ import AboutProject from "./AboutProject/AboutProject";
 import Techs from "./Techs/Techs";
 import AboutMe from "./AboutMe/AboutMe";
 import Portfolio from "./Portfolio/Portfolio";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Movies from "./Movies/Movies";
-import SavedMovies from "./SavedMovies/SavedMovies";
-import Register from "./Register/Register";
-import Login from "./Login/Login";
-import Profile from "./Profile/Profile";
 
 const Main = () => {
   return (
     <>
-      <Header />
-      {/* <Promo />
+      <Promo />
       <AboutProject />
       <Techs />
       <AboutMe />
       <Portfolio />
-      <Movies />
-      <SavedMovies />
-      <Footer />
-      <Register />
-      <Login /> */}
-      <Profile />
     </>
   )
 };
