d67a0caad2607e01b7ba4b7262b9fd44cab55a00
Vera Chernushina <chernushina.vera@gmail.com>
2022-06-16 22:46:09 +0300

    fix: Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,27 +4,29 @@ import Header from '../Header/Header';
 
 const Profile = () => {
   return (
-    <div className='profile__container'>
+    <section>
       <Header />
-      <h1 className='profile__title'>Привет, Вера!</h1>
-      <form className='profile___form'>
-        <div className='profile__value'>
-          <label className='profile__label'>Имя</label>
-          <input type='text' value='Вера' className='profile__input' />
+      <div className='profile__container'>
+        <h1 className='profile__title'>Привет, Вера!</h1>
+        <form className='profile___form'>
+          <div className='profile__value'>
+            <label className='profile__label'>Имя</label>
+            <input type='text' value='Вера' className='profile__input' />
+          </div>
+          <div className='profile__line'></div>
+          <div className='profile__value'>
+            <label className='profile__label'>
+              E-mail
+            </label>
+            <input type='email' value='someemail@yandex.ru' className='profile__input' />
+          </div>
+        </form>
+        <div className='profile__bottom'>
+          <button className='profile__edit'>Редактировать</button>
+          <button className='profile__logout'>Выйти из аккаунта</button>
         </div>
-        <div className='profile__line'></div>
-        <div className='profile__value'>
-          <label className='profile__label'>
-            E-mail
-          </label>
-          <input type='email' value='someemail@yandex.ru' className='profile__input' />
-        </div>
-      </form>
-      <div className='profile__bottom'>
-        <button className='profile__edit'>Редактировать</button>
-        <button className='profile__logout'>Выйти из аккаунта</button>
       </div>
-    </div>
+    </section>
   )
 };
 
