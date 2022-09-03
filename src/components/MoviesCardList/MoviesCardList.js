cbc9b68dc28076ccf44c8e49b5b071b2ead0fbe3
Vera Chernushina <chernushina.vera@gmail.com>
2022-09-03 19:39:30 +0300

    fix: window resize listener and cards list logic

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import useScreenWidth from '../../hooks/useScreenWidth';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 import { checkSavedCard } from '../../utils/utils';
@@ -22,36 +23,14 @@ const MoviesCardList = ({
   onDelete,
   isSavedMoviesPage
 }) => {
-  const [screenWidth, setScreenWidth] = useState(window.screen.width);
-  const [windowOuterWidth, setWindowOuterWidth] = useState(window.outerWidth);
+  const [showMovieList, setShowMovieList] = useState(movies);
+
+  const screenWidth = useScreenWidth();
 
   const searchedMoviesCount = movies ? movies.length : 0;
-  const bigScreenLayout = (screenWidth > BIG_SCREEN || windowOuterWidth > BIG_SCREEN) && searchedMoviesCount >= BIG_SCREEN_MOVIES_QTY;
-  const middleScreenLayout = ((screenWidth > SMALL_SCREEN && screenWidth <= BIG_SCREEN) || (windowOuterWidth > SMALL_SCREEN && windowOuterWidth <= BIG_SCREEN)) && searchedMoviesCount >= MIDDLE_SCREEN_MOVIES_QTY;
-  const smallScreenLayout = (screenWidth <= SMALL_SCREEN || windowOuterWidth <= SMALL_SCREEN) && searchedMoviesCount >= SMALL_SCREEN_MOVIES_QTY;
-
-  const [showMovieList, setShowMovieList] = useState(() => {
-    if (bigScreenLayout) {
-      return movies.slice(0, BIG_SCREEN_MOVIES_QTY);
-    } else if (middleScreenLayout) {
-      return movies.slice(0, MIDDLE_SCREEN_MOVIES_QTY);
-    } else if (smallScreenLayout) {
-      return movies.slice(0, SMALL_SCREEN_MOVIES_QTY);
-    } else {
-      return movies;
-    }
-  });
-
-  const traceScreenWidth = () => {
-    setScreenWidth(window.screen.width);
-  }
-
-  const traceWindowOuterWidth = () => {
-    setWindowOuterWidth(window.outerWidth);
-  }
 
   const handleMoreClick = () => {
-    if (screenWidth > BIG_SCREEN || windowOuterWidth > BIG_SCREEN) {
+    if (screenWidth > BIG_SCREEN) {
       setShowMovieList(movies.slice(0, showMovieList.length + MORE_MOVIES_BIG_SCREEN_QTY))
     } else {
       setShowMovieList(movies.slice(0, showMovieList.length + MORE_MOVIES_SMALL_SCREEN_QTY))
@@ -59,30 +38,16 @@ const MoviesCardList = ({
   }
 
   useEffect(() => {
-    window.addEventListener('resize', traceScreenWidth);
-    return () => {
-      window.removeEventListener('resize', traceScreenWidth)
-    }
-  }, [])
-
-  useEffect(() => {
-    window.addEventListener('resize', traceWindowOuterWidth)
-    return () => {
-      window.removeEventListener('resize', traceWindowOuterWidth)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (bigScreenLayout) {
+    if (screenWidth > BIG_SCREEN) {
       setShowMovieList(movies.slice(0, BIG_SCREEN_MOVIES_QTY))
-    } else if (middleScreenLayout) {
+    } else if (screenWidth > SMALL_SCREEN && screenWidth <= BIG_SCREEN) {
       setShowMovieList(movies.slice(0, MIDDLE_SCREEN_MOVIES_QTY));
-    } else if (smallScreenLayout) {
+    } else if (screenWidth <= SMALL_SCREEN) {
       setShowMovieList(movies.slice(0, SMALL_SCREEN_MOVIES_QTY));
     } else {
       setShowMovieList(movies);
     }
-  }, [screenWidth, windowOuterWidth, movies])
+  }, [screenWidth, movies])
 
   return (
     <section className='cards'>
